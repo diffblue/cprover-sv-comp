652b4347497276c13d47da1bdaa6bd482313106e
Michael Tautschnig <michael.tautschnig@qmul.ac.uk>
2017-02-24 14:46:25 +0000

    Only consider assignments to symbols

--- witness2test/process_witness.py
+++ witness2test/process_witness.py
@@ -222,7 +222,10 @@ def processWitness(witness, benchmark, bitwidth):
       if isinstance(a_ast, c_ast.Assignment):
         f = trace[n].get('assumption.scope')
         v = c_generator.CGenerator().visit(a_ast.rvalue)
-        if f is not None and inputs[f].get(a_ast.lvalue.name) is not None:
+        a_ast.show(showcoord=True)
+        if (f is not None and
+            isinstance(a_ast.lvalue, c_ast.ID) and
+            inputs[f].get(a_ast.lvalue.name) is not None):
           values.append([f, a_ast.lvalue.name, v])
         elif watch.get(int(trace[n]['startline'])) is not None:
           w = watch[int(trace[n]['startline'])]
